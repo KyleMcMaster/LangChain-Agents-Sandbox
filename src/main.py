45b19c93ae247753a68f46f102feef22fdcd5957
KyleMcMaster <11415127+KyleMcMaster@users.noreply.github.com>
2025-03-01 21:48:50 -0500

    add dotenv for development secrets

--- src/main.py
+++ src/main.py
@@ -1,29 +1,8 @@
-# Import relevant functionality
-from langchain_anthropic import ChatAnthropic
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_core.messages import HumanMessage
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+from chat import run_chat
 
-# Create the agent
-memory = MemorySaver()
-model = ChatAnthropic(model_name="claude-3-sonnet-20240229")
-search = TavilySearchResults(max_results=2)
-tools = [search]
-agent_executor = create_react_agent(model, tools, checkpointer=memory)
+# set environment variables from .env
+load_dotenv()
 
-# Use the agent
-config = {"configurable": {"thread_id": "abc123"}}
-for step in agent_executor.stream(
-    {"messages": [HumanMessage(content="hi im bob! and i live in sf")]},
-    config,
-    stream_mode="values",
-):
-    step["messages"][-1].pretty_print()
-
-for step in agent_executor.stream(
-    {"messages": [HumanMessage(content="whats the weather where I live?")]},
-    config,
-    stream_mode="values",
-):
-    step["messages"][-1].pretty_print()
+# run app
+run_chat()
